01630d24b31e52b180a2c85493bfdeb76b1f6c46
Sree Tej Peddaiahgari <sreetejpeddaiahgari@gmail.com>
2023-01-26 17:11:27 -0700

    created a project to use the dog api

---
+++ DogApi/DogApi/DogApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogApiApp.swift
+//  DogApi
+//
+//  Created by Sree Tej Peddaiahgari on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct DogApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
